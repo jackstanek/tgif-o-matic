316043a6c2eddb8ae331bab0bf7d9dd916a0aba9
Jack Stanek <jrstanek@wisc.edu>
2026-08-16 00:36:16 -0500

    add htmx and static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use axum::{Router, routing::get};
 use log::debug;
 use rand_chacha::rand_core::SeedableRng;
+use tower_http::services;
 
 use crate::config::BackendConfig;
 
 mod config;
 mod db;
 mod joincode;
+
+fn build_router() -> Router {
+    let serve_dir = services::ServeDir::new("assets");
+    Router::new()
+        .route("/", get(|| async { "Hello world!" }))
+        .nest_service("/assets", serve_dir)
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -22,7 +30,7 @@ async fn main() -> anyhow::Result<()> {
 
     db::init_admin_account(&config, &db, &mut rng).await?;
 
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+    let app = build_router();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
